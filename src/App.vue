e4ee9ddaae6bb30d9044581f72d26a53ac3eb2bb
KaKa <chienfang0611@gmail.com>
2020-09-08 00:10:49 +0800

    change title

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to jessica-portfolio.js App"/>
+    <HelloWorld msg="Welcome to Jessica's portfolio.js App"/>
   </div>
 </template>
 
