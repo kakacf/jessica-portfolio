7cf138a8ad211af73161ce348e70dcfb214502f7
KaKa <chienfang0611@gmail.com>
2020-09-25 00:44:56 +0800

    [Rearrange] Rearrange data to Json file

--- src/components/work/work.js
+++ src/components/work/work.js
@@ -1,8 +1,14 @@
+import myInfo from '../../data/myInfo.json';
+
 export default {
     name: 'work',
+    props: {
+        
+    },
     data() {
         return {
-            
+            title: myInfo.work
+        
         };
     },
     method: {
