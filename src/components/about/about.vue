bcb80023343cd0ad692fb6f72a25c125eb07fd34
KaKa <chienfang0611@gmail.com>
2020-09-25 00:49:21 +0800

    [Update] [about] Make content into separate file

--- src/components/about/about.vue
+++ src/components/about/about.vue
@@ -2,45 +2,72 @@
     <div>
         <div id="id-about" class="container-fluid">
             <div >
-                <h3 class="title">About</h3>
+                <h3 class="title" v-once>{{title}}</h3>
+            </div>
+
+            <div class="container">
+
+                <about-content v-bind:is-simple-tpl="true" v-bind:title="subTitleMe" v-bind:content="contentMe"></about-content>
+
+                <about-content v-bind:is-simple-tpl="false" v-bind:title="subTitleWorkExperiences">
+                    <div class="col">
+                        <div class="font-16">
+                            <span><b>Software Engineer @ 工樂行動科技</b></span>
+                        </div>
+                        <div class="font-14">
+                            <span>Mar.2017 - Now</span>
+                        </div>
+                    </div>
+                </about-content>
+
+                <about-content v-bind:is-simple-tpl="false" v-bind:title="subTitleEducation">
+                    <div class="col">
+                        <div class="font-16">
+                            <span><b>Master's Degree  @{{contentEducation.masterSchool}}</b></span>
+                        </div>
+                        <div class="font-14">
+                            <span>{{contentEducation.masterDuration}}</span>
+                        </div>
+                        <div class="font-14">
+                            <span>{{contentEducation.masterTitle}}</span>
+                        </div>
+                        <hr>
+                        <div class="font-16">
+                            <span><b>Bachelor's Degree  @{{contentEducation.undergraduateSchool}}</b></span>
+                        </div>
+                        <div class="font-14">
+                            <span>{{contentEducation.undergraduateDuration}}</span>
+                        </div>
+                        <div class="font-14">
+                            <span>{{contentEducation.undergraduateTtile}}</span>
+                        </div>
+                        <hr>
+                        <div class="font-16">
+                            <span><b>Specialized Degree  @{{contentEducation.specialSchool}}</b></span>
+                        </div>
+                        <div class="font-14">
+                            <span>{{contentEducation.specialDuration}}</span>
+                        </div>
+                        <div class="font-14">
+                            <span>{{contentEducation.specialTitle}}</span>
+                        </div>
+                        <p></p>
+                        <div class="font-14">
+                            <div v-for="(item, key, index) in ucomArray" :key="index">
+                                <span>{{key + 1}}. {{item}}</span>
+                            </div>
+                        </div>
+                    </div>
+                </about-content>
+
+                <!-- <about-content v-bind:is-simple-tpl="true" v-bind:title="subTitleSkill" v-bind:content="contentSkill"></about-content> -->
+
+                
+
+
             </div>
             
-            <div class="container">
-                <div class="row">
-                    <div class="col-1 row">
-                        <div class="dot"></div>
-                    </div>
-                    <div class="col-11">
-                        <p><b>Me</b></p>
-                        I come from a beautiful harbor city Kaohsiung. I was graduated from National Chiao Tung University and majoring in Cancer Biology.I am quite a fast-learner and adapt well to different environment, no matter what the challenges might be awaiting ahead of me. In my leisure time, I would love to explore new techniques and skills and traveling. Cooking and baking will always be my favorite hobbies when at home.
-                    </div>
-                </div>
-
-                <div class="row" >
-                    <div class="col-1 row">
-                    <div class="dot"></div>
-                    </div>
-                    <div class="col-11">
-                        <p><b>Work Experiences</b></p>
-                        I come from a beautiful harbor city Kaohsiung. I was graduated from National Chiao Tung University and majoring in Cancer Biology.I am quite a fast-learner and adapt well to different environment, no matter what the challenges might be awaiting ahead of me. In my leisure time, I would love to explore new techniques and skills and traveling. Cooking and baking will always be my favorite hobbies when at home.
-                    </div>
-                </div>
-
-            </div>
-
-            <!-- <div class="d-flex flex-column outer-div">
-                <div >
-                    I come from a beautiful harbor city Kaohsiung. I was graduated from National Chiao Tung University and majoring in Cancer Biology.I am quite a fast-learner and adapt well to different environment, no matter what the challenges might be awaiting ahead of me. In my leisure time, I would love to explore new techniques and skills and traveling. Cooking and baking will always be my favorite hobbies when at home.
-                </div>
-                <div class="margin-top-10">
-                    With my work experiences at Academia Sinica under Dr. Ying-Chih, Chang’s lab at the Genomic Research Center as research assistant, it has allowed me to fully utilized my analytical and quantitative reasoning that have been taught during my study. The main focus in Ying’s lab is researching and characterizing rare circulating tumor cells (CTCs). Microfluidic devices have been used in capturing these rare circulating tumour cells in order to improve the prognosis of cancer stages. This technique is successfully transferred to a company (CellMax Life). Due to my proficiency in this technique, I assisted them to set up the CTCs detection system.
-                </div>
-                <div class="margin-top-10">
-                    Working as research assistant is always challenging, however, computer programming will be another skills that I always want to explore and learn. This strong desirability has changed my life, in which I have started to take programming courses by 2016. After four months of hard-work, and thanks to the subsidy provided by Taiwan government, I have succeeded in creating my own apps and achieved another milestone in my life.
-                </div>
-            </div> -->
-
-
+            
         </div>
     </div>
 </template>
---
+++ src/components/aboutContent/aboutContent.js
@@ -0,0 +1,19 @@
+
+export default {
+    name: 'aboutContent',
+    props: {
+        title: String,
+        content: String,
+        isSimpleTpl: Boolean
+    },
+    data() {
+        return {
+        
+        };
+    },
+    method: {
+
+
+    }
+
+}
