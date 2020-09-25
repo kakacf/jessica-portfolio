463a9398218397915b3750e2e63c1efc30f117ba
Terry Liao <terry.liao@nogle.com>
2020-09-26 00:25:10 +0800

    optimize about me component

--- src/components/about/about.vue
+++ src/components/about/about.vue
@@ -1,75 +1,72 @@
 <template>
-    <div>
-        <div id="id-about" class="container-fluid">
-            <div >
-                <h3 class="title" v-once>{{title}}</h3>
+  <div>
+    <div id="id-about" class="container-fluid">
+      <div>
+        <h3 class="title" v-once>About</h3>
+      </div>
+
+      <div class="container">
+        <!-- me -->
+        <about-content
+          :is-simple-tpl="true"
+          :title="me.heading"
+          :content="me.content"
+        />
+        <!-- work experiences -->
+        <about-content :is-simple-tpl="false" :title="work.heading">
+          <div
+            class="col"
+            v-for="(experience, index) in work.experiences"
+            :key="index"
+          >
+            <div class="font-16">
+              <span
+                ><b>{{ experience.title }} @ {{ experience.company }}</b></span
+              >
             </div>
-
-            <div class="container">
-
-                <about-content v-bind:is-simple-tpl="true" v-bind:title="subTitleMe" v-bind:content="contentMe"></about-content>
-
-                <about-content v-bind:is-simple-tpl="false" v-bind:title="subTitleWorkExperiences">
-                    <div class="col">
-                        <div class="font-16">
-                            <span><b>Software Engineer @ 工樂行動科技</b></span>
-                        </div>
-                        <div class="font-14">
-                            <span>Mar.2017 - Now</span>
-                        </div>
-                    </div>
-                </about-content>
-
-                <about-content v-bind:is-simple-tpl="false" v-bind:title="subTitleEducation">
-                    <div class="col">
-                        <div class="font-16">
-                            <span><b>Master's Degree  @{{contentEducation.masterSchool}}</b></span>
-                        </div>
-                        <div class="font-14">
-                            <span>{{contentEducation.masterDuration}}</span>
-                        </div>
-                        <div class="font-14">
-                            <span>{{contentEducation.masterTitle}}</span>
-                        </div>
-                        <hr>
-                        <div class="font-16">
-                            <span><b>Bachelor's Degree  @{{contentEducation.undergraduateSchool}}</b></span>
-                        </div>
-                        <div class="font-14">
-                            <span>{{contentEducation.undergraduateDuration}}</span>
-                        </div>
-                        <div class="font-14">
-                            <span>{{contentEducation.undergraduateTtile}}</span>
-                        </div>
-                        <hr>
-                        <div class="font-16">
-                            <span><b>Specialized Degree  @{{contentEducation.specialSchool}}</b></span>
-                        </div>
-                        <div class="font-14">
-                            <span>{{contentEducation.specialDuration}}</span>
-                        </div>
-                        <div class="font-14">
-                            <span>{{contentEducation.specialTitle}}</span>
-                        </div>
-                        <p></p>
-                        <div class="font-14">
-                            <div v-for="(item, key, index) in ucomArray" :key="index">
-                                <span>{{key + 1}}. {{item}}</span>
-                            </div>
-                        </div>
-                    </div>
-                </about-content>
-
-                <!-- <about-content v-bind:is-simple-tpl="true" v-bind:title="subTitleSkill" v-bind:content="contentSkill"></about-content> -->
-
-                
-
-
+            <div class="font-14">
+              <span>{{ experience.year }}</span>
             </div>
-            
-            
-        </div>
+          </div>
+        </about-content>
+        <!-- education -->
+        <about-content
+          v-bind:is-simple-tpl="false"
+          v-bind:title="education.heading"
+        >
+          <div class="col">
+            <div
+              v-for="(education, index) in education.educations"
+              :key="index"
+            >
+              <div class="font-16">
+                <span
+                  ><b
+                    >{{ education.qualification }} @{{ education.school }}</b
+                  ></span
+                >
+              </div>
+              <div class="font-14">
+                <span>{{ education.location }} {{ education.year }}</span>
+              </div>
+              <div class="font-14">
+                <span>{{ education.major }}</span>
+              </div>
+              <hr v-if="!education.courses" />
+              <div v-if="education.courses" class="font-14">
+                <div
+                  v-for="(course, key, index) in education.courses"
+                  :key="index"
+                >
+                  <span>{{ key + 1 }}. {{ course }}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </about-content>
+      </div>
     </div>
+  </div>
 </template>
 
 <script src="./about.js"></script>
